0dcaa711a65c70960f177c61dc5d0806e9acc438
wikibootup <wikibootup@gmail.com>
2015-05-25 00:09:42 +0900

    Primitive-Data-Type

---
+++ Primitive-Data-Type-01.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+// Maximum Int value
+INT32_MAX
+UINT32_MAX
+
+
+// Type interface or Denotion
+var valentineDay = 214
+
+var whiteDay: Int = 314
+var c1: Character = "a"
+var c2: Character = "b"
+
+
+// String interpolation
+var codeName = "Kingsman"
+var phrase = "manners maketh man"
+var wordCount = 25
+let maxWordCount = 10000
+
+var message = "How many times \(codeName) said \(phrase)? more than \(wordCount)? Obiously, it was less than \(maxWordCount)"
+
+// Simple Tuple
+let simpleTuple = (1, 2, "one, two")
+
+simpleTuple.0
+simpleTuple.1
+simpleTuple.2
+
+
+// Key-value Tuple
+let keyValueTuple = (name: "Tim Cook", age: 54, phrase: "We're very simple people at Apple. We focus on making the world's best products and enriching people's lives.")
+
+println("\(keyValueTuple.name) is now Apple CEO. he's age is \"\(keyValueTuple.age). \nOne of his phrase is \(keyValueTuple.phrase)\"")
+
