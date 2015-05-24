d725befecda0a9dbfc2559c4e929a78bae5233f7
wikibootup <wikibootup@gmail.com>
2015-05-25 02:08:32 +0900

    Operator

---
+++ Operator.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+// Scope Operator
+var a = 97
+var z = 122
+
+a...z
+a..<z
+
+
+// Ternary Operator
+(99 > 11 ? 11 : 99)
+
+
+// Bit Operator
+var fifteen = 15
+var sixteen = 16
+
+fifteen & sixteen
+fifteen | sixteen
+fifteen ^ sixteen
+
+
+sixteen << 1
+fifteen << 1
