8ee9edd31a18f405826bfe9574f9d56b505ab102
wikibootup <wikibootup@gmail.com>
2015-05-24 18:39:56 +0900

    simple UI 01

---
+++ Simple-UI-01.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let myLabel = UILabel(frame: CGRectMake(0, 0, 200, 50))
+myLabel.backgroundColor = UIColor.redColor()
+myLabel.text = "Hello Swift"
+myLabel.textAlignment = .Center
+myLabel.font = UIFont(name: "Georgia", size: 24)
+myLabel
