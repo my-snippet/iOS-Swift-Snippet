1fbc11df7e9c6666e5651326406587c060688fdc
wikibootup <wikibootup@gmail.com>
2015-06-01 03:55:05 +0900

    init http library example

---
+++ HTTP-Networking-using-Alamofire/HTTP-Networking-using-Alamofire/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HTTP-Networking-using-Alamofire
+//
+//  Created by wikibootup on 6/1/15.
+//  Copyright (c) 2015 wikibootup. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
