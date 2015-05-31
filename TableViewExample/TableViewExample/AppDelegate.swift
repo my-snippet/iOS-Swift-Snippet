cceb4a219e0d90d653e60f29042fc3f68e6f6db4
wikibootup <wikibootup@gmail.com>
2015-05-31 14:45:28 +0900

    re init TableViewExample

--- TableViewExample/TableViewExample/AppDelegate.swift
+++ TableViewExample/TableViewExample/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  TableViewExample
 //
-//  Created by wikibootup on 5/30/15.
+//  Created by wikibootup on 5/31/15.
 //  Copyright (c) 2015 wikibootup. All rights reserved.
 //
 
--- TableViewExample/TableViewExampleTests/TableViewExampleTests.swift
+++ TableViewExample/TableViewExampleTests/TableViewExampleTests.swift
@@ -2,7 +2,7 @@
 //  TableViewExampleTests.swift
 //  TableViewExampleTests
 //
-//  Created by wikibootup on 5/30/15.
+//  Created by wikibootup on 5/31/15.
 //  Copyright (c) 2015 wikibootup. All rights reserved.
 //
 
